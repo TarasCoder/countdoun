41b40e5be112a45400fb1328fe164f0f76bee4e4
kaskad <kaskad@bigmir.net>
2021-06-21 23:23:41 +0300

    fix problem with 00 when switching from one minute to next

--- app.js
+++ app.js
@@ -12,21 +12,21 @@ btn.addEventListener("click", getData);
 
 function decreaseTime() {
 //   debugger;
+
   currentS--;
   if (currentS <= 0 && currentM == 0) {
     clearInterval(timerId);
     time.innerHTML = `0:00`;
     return;
-  }
-  if (currentS < 10 && currentS > 0) {
+  } else if (currentS < 10 && currentS > 0) {
     currentS = `0${currentS}`;
-  }
-  if (currentS === 0) {
-    //   debugger;
+  } else if (currentS === -1) {
     currentM--;
     currentS = 0;
-    time.innerHTML = `${currentM}:${currentS}`;
+    time.innerHTML = `${currentM}:00`;
     currentS = 59;
+  } else if (currentS === 0) {
+    currentS = "00";
   }
   time.innerHTML = `${currentM}:${currentS}`;
 }
@@ -34,7 +34,7 @@ function getData(event) {
   event.preventDefault();
   let min = document.querySelector("#minutes").value;
   let sec = document.querySelector("#seconds").value;
-//   debugger;
+  //   debugger;
   if (min == "" || sec == "") {
     alert("Please enter info in both fields!!!");
   } else {
